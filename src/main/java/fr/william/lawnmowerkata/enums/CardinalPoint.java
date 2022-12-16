7b5d972937f263da42f465fd9692e74eb0243136
WTCarbonIT <william.tran@carbon-it.com>
2022-12-16 11:52:53 +0100

    feat: create enums CardinalPoint and Movement

---
+++ src/main/java/fr/william/lawnmowerkata/enums/CardinalPoint.java
@@ -0,0 +1,8 @@
+package fr.william.lawnmowerkata.enums;
+
+public enum CardinalPoint {
+    NORTH,
+    EAST,
+    SOUTH,
+    WEST,
+}
---
+++ src/main/java/fr/william/lawnmowerkata/enums/Movement.java
@@ -0,0 +1,7 @@
+package fr.william.lawnmowerkata.enums;
+
+public enum Movement {
+    FORWARD,
+    LEFT,
+    RIGHT
+}
